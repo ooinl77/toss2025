8ac075931d0aa192ae12046c5d1cb9817a7ff979
ooinl77 <ooinl77@naver.com>
2025-04-08 23:53:00 +0900

    spring 연습

--- back/SpringBoot/Ex11H2DB/src/main/java/com/study/springboot/MainController.java
+++ back/SpringBoot/Ex11H2DB/src/main/java/com/study/springboot/MainController.java
@@ -1,4 +1,32 @@
-package com.study.springboot;
+package com.study.Pr01Counter;
 
-public class MainController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.w3c.dom.css.Counter;
+
+@Controller
+public class HtmlController {
+    //필드 주입
+    @Autowired
+    private Counter counter;
+
+    @GetMapping("/")
+    public String main(Model model){
+        model.addAttribute("count", counter.getCount());
+        return "index"; //index.html이 응답결과로 반환된다.
+    }
+    @GetMapping("/plus")
+    public String plus(Model model){
+        counter.setCount(counter.getCount() + 1);
+        model.addAttribute("count", counter.getCount());
+        return "index";
+    }
+    @GetMapping("/minus")
+    public String minus(Model model){
+        counter.setCount(counter.getCount() - 1);
+        model.addAttribute("count", counter.getCount());
+        return "index";
+    }
 }
